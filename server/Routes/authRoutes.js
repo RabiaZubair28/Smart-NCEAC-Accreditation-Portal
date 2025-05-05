75f86e35a6fa41dfe3c6474b0b626f8faece696e
Rabia Zubair <rabiazubair949@gmail.com>
2025-05-05 23:54:43 +0500

    final changes

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -1,12 +1,19 @@
 // authRoutes.js
 import mongoose from "mongoose";
 import express from "express";
-import { login,logout,studentLogin } from "../Controllers/authController.js"; // Named import
+import {
+  login,
+  logout,
+  studentLogin,
+  forgotPassword,
+  resetPassword,
+} from "../Controllers/authController.js"; // Named import
 const router = express.Router();
 
 router.post("/login", login);
-router.post('/student-login', studentLogin);
+router.post("/student-login", studentLogin);
 router.post("/logout", logout);
-
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
