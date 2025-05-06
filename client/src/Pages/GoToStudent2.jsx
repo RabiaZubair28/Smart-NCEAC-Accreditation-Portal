3ea75076e4e5ca52debbdd0dd0e9d44b8713058d
Rabia Zubair <rabiazubair949@gmail.com>
2025-05-06 13:51:23 +0500

    final changes

--- client/src/Pages/GoToStudent2.jsx
+++ client/src/Pages/GoToStudent2.jsx
@@ -26,7 +26,7 @@ const GoToStudent2 = () => {
       setError(null);
 
       const response = await fetch(
-        `http://localhost:1234/api/students/id/${id}`
+        `https://iba-nceac.site/api/students/id/${id}`
       );
       if (!response.ok) throw new Error("Failed to fetch student data");
 
@@ -55,7 +55,7 @@ const GoToStudent2 = () => {
   const updatePLOs = async (id) => {
     try {
       const response = await fetch(
-        `http://localhost:1234/api/students/${id}/updatePLO`,
+        `https://iba-nceac.site/api/students/${id}/updatePLO`,
         {
           method: "PUT",
           headers: {
@@ -170,7 +170,7 @@ const GoToStudent2 = () => {
 
       // Update server
       const response = await fetch(
-        `http://localhost:1234/api/students/${id}/courses/${currentCourse._id}/assessments/${currentAssessment._id}`,
+        `https://iba-nceac.site/api/students/${id}/courses/${currentCourse._id}/assessments/${currentAssessment._id}`,
         {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
